6d581a5fea9f52325ad86228c0f8095ec29d5b8b
Marc <marc.ferraro1@gmail.com>
2021-02-03 19:27:26 -0600

    commit tree f37c75a9e95cf99586fa46d59e798f7a886a5d9b

---
+++ js/challenge.js
@@ -0,0 +1,10 @@
+function ticker(){
+    const counter = document.getElementById('counter')
+
+    while (true){
+        // upTick()
+    }
+}
+
+
+//set while loop to truthiness of pause button function
