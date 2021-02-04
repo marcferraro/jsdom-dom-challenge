eb6b50e8f411f1200ca62bbed2869a9755b5d857
Marc <marc.ferraro1@gmail.com>
2021-02-03 20:13:36 -0600

    Resume

--- js/challenge.js
+++ js/challenge.js
@@ -1,10 +1,29 @@
-function ticker(){
-    const counter = document.getElementById('counter')
-
-    while (true){
-        // upTick()
-    }
+function main(){
+    createPauseListener()
 }
 
+const counter = document.getElementById('counter')
+let tickerId = window.setInterval(uptick, 1000);
 
-//set while loop to truthiness of pause button function
+function uptick(){
+        counter.innerText = parseInt(counter.innerText, 10) + 1
+        console.log("counter increased by one")
+}
+
+function createPauseListener(){
+    const pause = document.getElementById("pause")
+
+    pause.addEventListener('click', function(e){
+        if (pause.id==="pause"){
+            clearInterval(tickerId);
+            pause.innerText="resume";
+            pause.id="resume";
+        } else {
+            tickerId = window.setInterval(uptick, 1000);
+            pause.innerText="pause";
+            pause.id="pause";
+        }
+    })
+}
+
+main()
